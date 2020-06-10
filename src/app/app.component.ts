ab81df5d8b885226ea8c0208490136f6478ffcaa
Dominik Martin <dominik.martin@kit.edu>
2020-06-10 15:46:46 +0200

    meta data cards / counts

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,6 +7,17 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   userInfoToggle = false;
+  articles = 0;
+  words = 0;
+  db = 0;
+  progress = 0;
+
+  metaInfo = {
+    articles: 1850,
+    words: 148000,
+    databases: 5,
+    progress: 100
+  };
 
   toggleUserInfo() {
     this.userInfoToggle = !this.userInfoToggle;
